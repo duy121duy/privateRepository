b17ef61fc07aef5562fc70ce8ca780e1a0679045
duy121duy <54900471+duy121duy@users.noreply.github.com>
2022-01-05 22:01:03 +0700

    Update Web

--- BaiTap2_3/src/main/java/com/example/baitap2_3/service/FindPrimeNumberService.java
+++ BaiTap2_3/src/main/java/com/example/baitap2_3/service/FindPrimeNumberService.java
@@ -3,5 +3,6 @@ package com.example.baitap2_3.service;
 import java.util.List;
 
 public interface FindPrimeNumberService {
+    /* Chinh sua tren server github */
     public List<Integer> findPrimeNumber(int n);
 }
